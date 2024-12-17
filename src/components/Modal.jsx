257ca8402455baab9e036b08fffbd91f2f0f0f24
Amshekhar <engineer.shekhar10@gmail.com>
2024-12-17 23:48:20 +0530

    code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import Button from './Button';
+import { FaTimes } from 'react-icons/fa';
 
 const Modal = ({ isOpen, title, children, onClose }) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-4  rounded w-96">
+      <div className="bg-white p-4 rounded w-96 relative">
+        <button
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          onClick={onClose}
+        >
+          <FaTimes size={20} />
+        </button>
         <h2 className="text-xl mb-4">{title}</h2>
         {children}
-        <div className="flex justify-end mt-4">
-          <Button text="Close" onClick={onClose} variant="secondary" />
-        </div>
       </div>
     </div>
   );
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -133,7 +133,7 @@ const Favorites = () => {
         title={`Details for ${viewPackage.packageName}`}
         onClose={() => setShowViewModal(false)}
       >
-        <p className="text-gray-700">{viewPackage.reason}</p>
+        <p className="text-gray-700 overflow-auto">{viewPackage.reason}</p>
         <div className="flex justify-end mt-4">
           <button
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
